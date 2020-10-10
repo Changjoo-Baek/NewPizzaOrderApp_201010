72f2a89dfb868b47e388b4cb341b6fce9f55dc70
bucephalus41 <bucephalus41@gmail.com>
2020-10-10 09:29:45 +0900

    밀린 복습중

---
+++ app/src/main/java/com/zzazzu/newpizzaorderapp_201010/Adapter/MainViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.zzazzu.newpizzaorderapp_201010.Adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+    import androidx.fragment.app.Fragment
+    import androidx.fragment.app.FragmentManager
+    import androidx.fragment.app.FragmentPagerAdapter
+\\    import kr.co.tjoeun.pizzaorderapp_20200926.fragments.MyProfileFragment
+\\import kr.co.tjoeun.pizzaorderapp_20200926.fragments.PizzaStoreFragment
+
+    class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getPageTitle(position: Int): CharSequence? {
+
+            return when(position) {
+                0 -> "피자 주문"
+                else -> "내 정보 설정"
+            }
+
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return when(position) {
+                0 -> PizzaStoreFragment()
+                else -> MyProfileFragment()
+            }
+        }
+
+        override fun getCount(): Int {
+            return 2
+        }
+    }
--- app/src/main/java/com/zzazzu/newpizzaorderapp_201010/MainActivity.kt
+++ app/src/main/java/com/zzazzu/newpizzaorderapp_201010/MainActivity.kt
@@ -3,9 +3,31 @@ package com.zzazzu.newpizzaorderapp_201010
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var mViewPagerAdapter: MainViewPagerAdapter
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            setupEvents()
+            setValues()
+        }
+
+
+        override fun setupEvents() {
+
+        }
+
+        override fun setValues() {
+
+            mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+            mainViewPager.adapter = mViewPagerAdapter
+
+            mainTabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
